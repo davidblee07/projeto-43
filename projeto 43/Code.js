2e355fde12d9d97858f5dfd26cdfb97daaa1c0fa
davidblee07 <93348233+davidblee07@users.noreply.github.com>
2022-05-23 14:31:05 -0300

    Add files via upload

---
+++ projeto 43/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+// adicione código para exibir o restante dos nomes e as dicas.
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Dica: Forma uma certa função!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Dica: Armazena todas as informações!!", 100,270)
+
+}
